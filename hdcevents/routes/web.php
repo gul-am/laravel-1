a87200e72074d66a5c761880f9e724d05f50a29b
gul-amd-moz <100712631+gul-amd-moz@users.noreply.github.com>
2022-03-15 14:13:08 +0200

    ongoing
    yesterday exercise

--- hdcevents/routes/web.php
+++ hdcevents/routes/web.php
@@ -14,5 +14,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+    $nome = "Gulamo";
+
+    return view('welcome', ['nome' => $nome]);
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/produtos', function () {
+    return view('products');
 });
