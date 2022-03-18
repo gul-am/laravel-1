f7351aa4482072a0ef860738c8a08ac310ec1cd6
gul-amd-moz <100712631+gul-amd-moz@users.noreply.github.com>
2022-03-18 11:41:55 +0200

    criacao de navbar
    introducao de parametros e criacao do da bara de navegador.

--- hdcevents/resources/views/layouts/main.blade.php
+++ hdcevents/resources/views/layouts/main.blade.php
@@ -18,10 +18,36 @@
             crossorigin="anonymous">
             
         <!-- CSS da aplicacao -->
-        <link rel="stylesheet" href="/css/styles.css">
-        <script src="/js/scripts.js"></script>        
+            <link rel="stylesheet" href="/css/styles.css">
+            <script src="/js/scripts.js"></script>   
+    
     </head>
     <body>
+
+        <header>
+            <nav class="navbar navbar-expand-lg navbar-light" >
+                <div class="collapse navbar-collapse" id="navbar">
+                    <a href="/" class="navbar-brand">
+                     <img src="/img/hdcevents_logo.svg" alt="HDC Events">
+                    </a>
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a href="/" class="nav-link">Eventos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="/" class="nav-link">Criar Eventos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="/" class="nav-link">Entrar</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="/" class="nav-link">Cadastrar</a>
+                        </li>
+                    </ul>
+
+                 </div>
+        </header>      
+
         @yield('content')
         <footer>
             <p>HDC Events &copy; 2020</p>
