ec8e0b76043e874e5166ae031b5cf85565ea3579
gul-amd-moz <100712631+gul-amd-moz@users.noreply.github.com>
2022-03-21 21:34:13 +0200

    Aula 13
    inserir dados diretamente na base de dados e publicar os dados na pagina inicial atraves do controller...

--- hdcevents/resources/views/welcome.blade.php
+++ hdcevents/resources/views/welcome.blade.php
@@ -4,18 +4,8 @@
     
 @section('content')
 
-<h1>Algumm titulo</h1>
-<img src="/img/banner.jpg" alt="Banner">
-@if(10 > 5)
-    <p>A condicao e verdadeira</p>
-@endif
-
-<p>{{ $nome }}</p>
-
-@if($nome == "Amade")
-<p>O nome e Amade</p>            
-@else
-<p>O nome nao e Amade</p>
-@endif
+@foreach($events as $event)
+    <p>{{ $event->title }} -- {{ $event->description }}</p>
+@endforeach
 
 @endsection
